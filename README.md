e43f829cc25a5b32493ad055c778f78b11ffbc30
SurajSingh7 <220suraj@gmail.com>
2023-08-18 01:28:37 +0530

    Start Frontend Part

---
+++ README.md
@@ -0,0 +1,3 @@
+# React & Tailwind CSS Starter Pack
+
+This is a starter pack for creating React projects with Tailwind CSS configured. It uses React version **18.2** and Tailwind CSS version **3.2**.
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// /server/node_modules
+
+
+import App from "./App";
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+
