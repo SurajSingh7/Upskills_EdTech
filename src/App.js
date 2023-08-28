3b757655947a56eac2c4cdcb405492af75b946e8
SurajSingh7 <220suraj@gmail.com>
2023-08-28 23:31:42 +0530

    Auth sendotp verification

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Signup from "./pages/Signup"
 
 // import ForgotPassword from "./pages/ForgotPassword";
 // import UpdatePassword from "./pages/UpdatePassword";
-// import VerifyEmail from "./pages/VerifyEmail";
+import VerifyEmail from "./pages/VerifyEmail";
 // import About from "./pages/About";
 // import Contact from "./pages/Contact";
 // import MyProfile from "./components/core/Dashboard/MyProfile";
@@ -67,7 +67,17 @@ function App() {
             </OpenRoute>
           }
         />
+      <Route
+          path="verify-email"
+          element={
+            <OpenRoute>
+              <VerifyEmail />
+            </OpenRoute>
+          }
+        />  
 
+        
+      
 
     <Route path="*" element={<Error />} />
 
