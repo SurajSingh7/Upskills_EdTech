4f3b828ab076c58082d5d2f76b8e0a9ebdd06dde
SurajSingh7 <220suraj@gmail.com>
2023-08-30 00:35:54 +0530

    Profiledropdown & logout Auth done

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -95,9 +95,10 @@ exports.signup = async (req, res) => {
       accountType: accountType,
       approved: approved,
       additionalDetails: profileDetails._id,
-      image: "https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}",
+      image: `https://api.dicebear.com/5.x/initials/svg?seed=${firstName }${" "}${lastName}`,
     })
 
+   
     return res.status(200).json({
       success: true,
       user,
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -3,8 +3,7 @@
 // 3.Login,SingUp,ProfileDropdown,cart
 
 import { useEffect, useState } from "react"
-import { AiOutlineMenu } from "react-icons/ai"
-// import { AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai"
+import { AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai"
 import { BsChevronDown } from "react-icons/bs"
 import { useSelector } from "react-redux"
 import { Link, matchPath, useLocation } from "react-router-dom"
@@ -13,13 +12,13 @@ import logo from "../../assets/Logo/Logo-Full-Light.png"
 import { NavbarLinks } from "../../data/navbar-links"
 import { apiConnector } from "../../services/apiconnector"
 import { categories } from "../../services/apis"
-// import { ACCOUNT_TYPE } from "../../utils/constants"
+import { ACCOUNT_TYPE } from "../../utils/constants"
 import ProfileDropdown from "../core/Auth/ProfileDropDown"
 
 function Navbar() {
   const { token } = useSelector((state) => state.auth)
-  // const { user } = useSelector((state) => state.profile)
-  // const { totalItems } = useSelector((state) => state.cart)
+  const { user } = useSelector((state) => state.profile)
+  const { totalItems } = useSelector((state) => state.cart)
   const location = useLocation()
 
   const [subLinks, setSubLinks] = useState([])
@@ -117,21 +116,23 @@ function Navbar() {
             ))}
           </ul>
         </nav>
+
         {/* Login / Signup / Dashboard */}
         <div className="hidden items-center gap-x-4 md:flex">
 
           {
          
-          // user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
-          //   <Link to="/dashboard/cart" className="relative">
-          //     <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
-          //     {totalItems > 0 && (
-          //       <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-yellow-100">
-          //         {totalItems}
-          //       </span>
-          //     )}
-          //   </Link>
-          // )
+        //red@@@@@@@@@@@@
+         token && user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
+            <Link to="/dashboard/cart" className="relative">
+              <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
+              {totalItems > 0 && (
+                <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-yellow-100">
+                  {totalItems}
+                </span>
+              )}
+            </Link>
+          )
           
           }
           {token === null && (
