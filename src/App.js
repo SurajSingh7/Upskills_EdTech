9e7bfeda169305e6d72382382356b0c4f201dbea
SurajSingh7 <220suraj@gmail.com>
2023-08-29 13:45:40 +0530

    Forgot Password done

--- src/App.js
+++ src/App.js
@@ -9,38 +9,28 @@ import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 
 
-// import ForgotPassword from "./pages/ForgotPassword";
-// import UpdatePassword from "./pages/UpdatePassword";
+import ForgotPassword from "./pages/ForgotPassword";
+import UpdatePassword from "./pages/UpdatePassword";
 import VerifyEmail from "./pages/VerifyEmail";
+
 // import About from "./pages/About";
 // import Contact from "./pages/Contact";
 // import MyProfile from "./components/core/Dashboard/MyProfile";
 // import Dashboard from "./pages/Dashboard";
 // import PrivateRoute from "./components/core/Auth/PrivateRoute";
+
 import Error from "./pages/Error"
 // import Settings from "./components/core/Dashboard/Settings";
 import { useDispatch, useSelector } from "react-redux";
 
-// import EnrolledCourses from "./components/core/Dashboard/EnrolledCourses";
-// import Cart from "./components/core/Dashboard/Cart";
+// 2.
 
-import { ACCOUNT_TYPE } from "./utils/constants";
 
-// import AddCourse from "./components/core/Dashboard/AddCourse";
-// import MyCourses from "./components/core/Dashboard/MyCourses";
-// import EditCourse from "./components/core/Dashboard/EditCourse";
-// import Catalog from "./pages/Catalog";
-// import CourseDetails from "./pages/CourseDetails";
-// import ViewCourse from "./pages/ViewCourse";
-// import VideoDetails from "./components/core/ViewCourse/VideoDetails";
-// import Instructor from "./components/core/Dashboard/InstructorDashboard/Instructor";
 
 function App() {
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
   
-  const { user } = useSelector((state) => state.profile)
+  // const { user } = useSelector((state) => state.profile)
 
 
   return (
@@ -74,12 +64,27 @@ function App() {
               <VerifyEmail />
             </OpenRoute>
           }
-        />  
-
-        
+        />
       
+      <Route
+          path="update-password/:id"
+          element={
+            <OpenRoute>
+              <UpdatePassword />
+            </OpenRoute>
+          }
+        /> 
 
-    <Route path="*" element={<Error />} />
+       <Route
+          path="forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword />
+            </OpenRoute>
+          }
+        />
+
+      <Route path="*" element={<Error />} />
 
 
     </Routes>
--- src/services/operations/authAPI.js
+++ src/services/operations/authAPI.js
@@ -157,25 +157,30 @@ export function getPasswordResetToken(email , setEmailSent) {
   }
 }
 
-export function resetPassword(password, confirmPassword, token) {
-  return async(dispatch) => {
-    dispatch(setLoading(true));
-    try{
-      const response = await apiConnector("POST", RESETPASSWORD_API, {password, confirmPassword, token});
+export function resetPassword(password, confirmPassword, token, navigate) {
+  return async (dispatch) => {
+    const toastId = toast.loading("Loading...")
+    dispatch(setLoading(true))
+    try {
+      const response = await apiConnector("POST", RESETPASSWORD_API, {
+        password,
+        confirmPassword,
+        token,
+      })
 
-      console.log("RESET Password RESPONSE ... ", response);
+      console.log("RESETPASSWORD RESPONSE............", response)
 
-
-      if(!response.data.success) {
-        throw new Error(response.data.message);
+      if (!response.data.success) {
+        throw new Error(response.data.message)
       }
 
-      toast.success("Password has been reset successfully");
+      toast.success("Password Reset Successfully")
+      navigate("/login")
+    } catch (error) {
+      console.log("RESETPASSWORD ERROR............", error)
+      toast.error("Failed To Reset Password")
     }
-    catch(error) {
-      console.log("RESET PASSWORD TOKEN Error", error);
-      toast.error("Unable to reset password");
-    }
-    dispatch(setLoading(false));
+    toast.dismiss(toastId)
+    dispatch(setLoading(false))
   }
 }
