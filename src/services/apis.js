cadee8be6c033460ec2cd975eaa8ee42247ad461
SurajSingh7 <220suraj@gmail.com>
2023-09-25 17:28:22 +0530

    Backend part seperation & deploy

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,6 @@
-const BASE_URL = "http://localhost:4000/api/v1"
+// const BASE_URL = "http://localhost:4000/api/v1"
+
+const BASE_URL = "https://upskills-backend.vercel.app/api/v1";
 
 // AUTH ENDPOINTS
 export const endpoints = {
